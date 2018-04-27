09b0f2206bc7322d84acab67d878c402b17d188d
Jake-Tibbits <jake@bonusmedia.co.uk>
2018-04-27 12:46:15 +0100

    auto-indent app.js using prettier playground

--- js/app.js
+++ js/app.js
@@ -1,19 +1,18 @@
 /*jshint esversion: 6 */
 
-
 //DEFINING VARIABLES
 
 //get the deck
-const deck = document.querySelector('.deck');
-const deckPicker = document.getElementById('deckPicker');
+const deck = document.querySelector(".deck");
+const deckPicker = document.getElementById("deckPicker");
 
 //create an empty array to hold all cards
 let cards = [];
 
 //populate the array with cards elements and give each card a click eventListener
-deck.querySelectorAll('.card').forEach(function(card) {
+deck.querySelectorAll(".card").forEach(function(card) {
   cards.push(card);
-  card.addEventListener('click', clickCard);
+  card.addEventListener("click", clickCard);
 });
 
 // declare counter to keep track of number of moves
@@ -36,22 +35,23 @@ const winModal = document.querySelector(".modal");
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
   while (currentIndex !== 0) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
   }
 
   return array;
 }
 
 //define function to shuffle and deal the cards
-function refreshCards(){
-
+function refreshCards() {
   //hide the deck for to avoid reflows
   deck.hidden = true;
 
@@ -62,7 +62,7 @@ function refreshCards(){
 
   //re-order the cards using flex order
   let i = 0;
-  cards.forEach(function(card){
+  cards.forEach(function(card) {
     i++;
     card.style.order = i;
     //reset all cards to initial state
@@ -73,40 +73,31 @@ function refreshCards(){
   deck.hidden = false;
 
   //reset the moveCounter
-  incrementCounter('reset');
+  incrementCounter("reset");
 
   //close any open cards
   openCards = [];
 
-
   counter = 0;
   endGame(0, false);
 
-
-
   //if modal is open, hide it
-  winModal.removeAttribute('open');
-
+  winModal.removeAttribute("open");
 }
 
 //refresh cards every time the page is loaded so that the starting order is always different
 document.addEventListener("DOMContentLoaded", refreshCards);
 
 //refresh cards when restart button is clicked
-document.querySelector('.restart').addEventListener('click', refreshCards);
-winModal.querySelector('.restart').addEventListener('click', refreshCards);
-winModal.querySelector('.exit').addEventListener('click', refreshCards);
-
-
-
-
+document.querySelector(".restart").addEventListener("click", refreshCards);
+winModal.querySelector(".restart").addEventListener("click", refreshCards);
+winModal.querySelector(".exit").addEventListener("click", refreshCards);
 
 //DEFINING CARD CLICK BEHAVIOURS
 
 //declare function to fire when card is clicked
 function clickCard() {
-
-  if(movesCounter === 0){
+  if (movesCounter === 0) {
     //startingTime = performance.now();
     startTimer();
   }
@@ -117,25 +108,22 @@ function clickCard() {
   //check for a match
   const cardCheck = checkCards();
 
-  if(cardCheck == "match"){
+  if (cardCheck == "match") {
     matchCards();
     //check if all cards are matched
-    if(matchedCards.length == cards.length){
+    if (matchedCards.length == cards.length) {
       //do a win
       endGame(time, true);
     }
-  } else if(cardCheck == "noMatch") {
+  } else if (cardCheck == "noMatch") {
     //reset unmatched cards
     unFlipCards();
   }
-
 }
 
-
 //function to trigger win
-function endGame(time, win){
-
-  if(win){
+function endGame(time, win) {
+  if (win) {
     const open = document.createAttribute("open");
     winModal.setAttributeNode(open);
     clearInterval(timer);
@@ -148,24 +136,19 @@ function endGame(time, win){
   timerDisplay.innerText = time;
 
   winModal.querySelector(".timer").innerText = time;
-
 }
-
-
-
 
 function startTimer() {
-  timer = setInterval(function(){
+  timer = setInterval(function() {
     incrementTime();
-  },1000);
+  }, 1000);
 }
 
-function incrementTime(){
+function incrementTime() {
   counter++;
   time = timeFormat(counter * 1000);
   timerDisplay.innerText = time;
 }
-
 
 //function to convert milliseconds to readable time - found at https://stackoverflow.com/questions/9763441/milliseconds-to-time-in-javascript?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 function timeFormat(time) {
@@ -175,144 +158,133 @@ function timeFormat(time) {
   time = (time - secs) / 60;
   var mins = time % 60;
 
-  return mins + ' mins ' + secs + ' secs';
+  return mins + " mins " + secs + " secs";
 }
-
 
 //function to flip a card on click
-function flipCard(card){
-
+function flipCard(card) {
   //add open card styles
-  card.classList.add('open', 'show');
+  card.classList.add("open", "show");
   //add card to openCards array
   openCards.push(card);
-
 }
 
-
 //function to reset cards if no match found
-function unFlipCards(){
-
+function unFlipCards() {
   //make other cards unclickable during delay
   deck.style.pointerEvents = "none";
 
   //add a delay to give people time to memorize the cards
-  setTimeout(function(){
-    openCards.forEach(function(openCard){
+  setTimeout(function() {
+    openCards.forEach(function(openCard) {
       //remove open card styles
-      openCard.classList.remove('open', 'show');
+      openCard.classList.remove("open", "show");
       //reset openCards array
       openCards = [];
       //re-endable clicking
       deck.style.pointerEvents = "auto";
     });
   }, 1200);
-
 }
 
 //function to set cards to matched state
-function matchCards(){
-
-  openCards.forEach(function(openCard){
+function matchCards() {
+  openCards.forEach(function(openCard) {
     //set matched card class and remove open card classes
-    openCard.classList.add('match');
-    openCard.classList.remove('open', 'show');
+    openCard.classList.add("match");
+    openCard.classList.remove("open", "show");
 
     //add cards to matchedCards array
     matchedCards.push(openCard);
   });
   //reset openCards array
   openCards = [];
-
 }
 
 //function to increment or reset the movesCounter
-function incrementCounter(reset){
+function incrementCounter(reset) {
   //define variables for outputting score to DOM
-  const scorePanel = document.querySelector('.score-panel');
-  const movesSpan = scorePanel.querySelector('.moves');
-  const topStars = scorePanel.querySelectorAll('.stars li');
-  const modalStars = winModal.querySelectorAll('.stars li');
+  const scorePanel = document.querySelector(".score-panel");
+  const movesSpan = scorePanel.querySelector(".moves");
+  const topStars = scorePanel.querySelectorAll(".stars li");
+  const modalStars = winModal.querySelectorAll(".stars li");
 
   //hide scorePanel to avoid reflows
   scorePanel.hidden = true;
 
   //check if counter is to be increased or reset
-  if (reset){
+  if (reset) {
     //reset counter
     movesCounter = 0;
     //reset stars
-    topStars.forEach(function(star){
-      star.classList= "";
+    topStars.forEach(function(star) {
+      star.classList = "";
     });
-    modalStars.forEach(function(star){
-      star.classList= "";
+    modalStars.forEach(function(star) {
+      star.classList = "";
     });
     movesSpan.innerText = movesCounter;
   } else {
     //increment counter
-    movesCounter +=0.5;
+    movesCounter += 0.5;
     //console.log(movesCounter);
-    if(movesCounter == 14){
-      topStars[0].classList.add('hidden');
-      modalStars[0].classList.add('hidden');
-    } else if (movesCounter == 20){
-      topStars[2].classList.add('hidden');
-      modalStars[2].classList.add('hidden');
+    if (movesCounter == 14) {
+      topStars[0].classList.add("hidden");
+      modalStars[0].classList.add("hidden");
+    } else if (movesCounter == 20) {
+      topStars[2].classList.add("hidden");
+      modalStars[2].classList.add("hidden");
     }
   }
 
-  if(openCards.length === 2){
+  if (openCards.length === 2) {
     //update moves total
     movesSpan.innerText = movesCounter;
   }
 
   //unhide scorePanel to after changes made
   scorePanel.hidden = false;
-
 }
-
 
 function checkCards() {
   //increment the move counter
   incrementCounter();
 
   //check if two cards are open
-  if(openCards.length === 2){
-
-
+  if (openCards.length === 2) {
     //check if the open cards match
-    if(openCards[0].getAttribute("data-pair") == openCards[1].getAttribute("data-pair")){
+    if (
+      openCards[0].getAttribute("data-pair") ==
+      openCards[1].getAttribute("data-pair")
+    ) {
       return "match";
     } else {
       return "noMatch";
     }
-
   }
-
 }
 
 //add listener to trigger event when a different deck is chosen
-deckPicker.addEventListener('change', refreshCards);
+deckPicker.addEventListener("change", refreshCards);
 
-function setDeck(){
+function setDeck() {
   //find out which deck was chosen
   const chosenDeck = deckPicker.options[deckPicker.selectedIndex].value;
   //change the background image to match the new deck
-  document.querySelector('body').style.backgroundImage = "url('img/"+chosenDeck+"/bg.jpg')";
+  document.querySelector("body").style.backgroundImage =
+    "url('img/" + chosenDeck + "/bg.jpg')";
   //give the deck a class to contain any styles for the chosen deck
-  deck.classList = "deck "+chosenDeck;
+  deck.classList = "deck " + chosenDeck;
 
   //loop through cards
-  cards.forEach(function(card){
+  cards.forEach(function(card) {
     //find out which data-pair the card belongs to
     const letter = card.getAttribute("data-pair");
     //give the card the appropriate styling for the deck and data-pair
-    card.querySelector(".back").style.backgroundImage = "url(img/"+chosenDeck+"/reverse.svg)";
-    card.querySelector(".front").classList = "front "+chosenDeck;
-    card.querySelector(".front").style.backgroundImage = "url(img/"+chosenDeck+"/"+letter+".svg)";
-
+    card.querySelector(".back").style.backgroundImage =
+      "url(img/" + chosenDeck + "/reverse.svg)";
+    card.querySelector(".front").classList = "front " + chosenDeck;
+    card.querySelector(".front").style.backgroundImage =
+      "url(img/" + chosenDeck + "/" + letter + ".svg)";
   });
-
-
 }
